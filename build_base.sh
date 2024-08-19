77c32e266c3b7bdc1677a8500d73d8a903bdd339
kowalskyyy999 <rionugroho70@gmail.com>
2024-08-19 17:45:09 +0700

    Fix: revamp the pipeline

--- build_base.sh
+++ build_base.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 set -e
+. ./.env
+
+# wget https://releases.starrocks.io/starrocks/StarRocks-${STARROCKS_VERSION}.tar.gz -O ./image/base/StarRocks-${STARROCKS_VERSION}.tar.gz
 
 docker build \
-    -t starrocks-base:3.1.6 \
+    --build-arg STARROCKS_VERSION=${STARROCKS_VERSION} \
+    --build-arg PRIORITY_NETWORKS=${PRIORITY_NETWORKS} \
+    -t starrocks-base:$STARROCKS_VERSION \
     -f ./image/base/Dockerfile .
