37dcf6eb4f21b17804d9c6354e7043d590d1270b
kowalskyyy999 <rionugroho70@gmail.com>
2024-08-19 18:08:25 +0700

    Fix: add starrocks base url

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 . ./.env
 
-#wget https://releases.starrocks.io/starrocks/StarRocks-${STARROCKS_VERSION}.tar.gz -O ./image/base/StarRocks-${STARROCKS_VERSION}.tar.gz
+wget https://releases.starrocks.io/starrocks/StarRocks-${STARROCKS_VERSION}.tar.gz -O ./image/base/StarRocks-${STARROCKS_VERSION}.tar.gz
 
 docker compose up -d --build 
 sleep 10 
